d74a5bd220a7eb944f9e38a1940ecfb4a9f6b1f9
ValueLan <332716137@qq.com>
2019-03-28 09:46:45 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,60 +1,70 @@
-const utils =  {
-	formatDate(datetime, format) {
-		if (!format) format = 'YYYY-MM-DD HH:mm';
-		if (typeof(datetime) == 'string') {
-			datetime = datetime.replace(/\-/g, '/');
-			datetime = new Date(datetime);
-		} else if (typeof(datetime) == 'number') {
-			datetime = new Date(datetime * 1000);
-		} else if (!(datetime instanceof Date)) {
-			datetime = new Date();
-		}
+const utils = {
+  formatDate(datetime, format) {
+    if (!format) format = 'YYYY-MM-DD HH:mm';
+    if (typeof(datetime) == 'string') {
+      datetime = datetime.replace(/\-/g, '/');
+      datetime = new Date(datetime);
+    } else if (typeof(datetime) == 'number') {
+      datetime = new Date(datetime * 1000);
+    } else if (!(datetime instanceof Date)) {
+      datetime = new Date();
+    }
 
-		var week = ['日', '一', '二', '三', '四', '五', '六'];
-		return format.replace(/YYYY|YY|MM|DD|HH|hh|mm|SS|ss|week/g, function(key) {
-			switch (key) {
-				case 'YYYY': return datetime.getFullYear();
-				case 'YY': return (datetime.getFullYear() + '').slice(2);
-				case 'MM': return datetime.getMonth() + 1 < 10 ? '0' + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
-				case 'DD': return datetime.getDate() < 10 ? '0' + datetime.getDate() : datetime.getDate();
-				case 'HH':
-				case 'hh': return datetime.getHours() < 10 ? '0' + datetime.getHours() : datetime.getHours();
-				case 'mm': return datetime.getMinutes() < 10 ? '0' + datetime.getMinutes() : datetime.getMinutes();
-				case 'SS':
-				case 'ss': return datetime.getSeconds() < 10 ? '0' + datetime.getSeconds() : datetime.getSeconds();
-				case 'week': return week[datetime.getDay()];
-			}
-		});
-	},
-	dataReplace(str, data) {
-		return str.replace(/{([^}]+)}/g, function(item, $1) {
-			if (data[$1]) return data[$1];
-			return item
-		});
-	},
-	formatCoin(number) {
-		number = (number * 1 || 0);
-		return (number.toFixed(2) + '').replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
-	},
-	getBase64(file, callback) {
-		var reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onload = function(e) {
-			typeof(callback) == 'function' && callback(e.currentTarget.result);
-		}
-	},
-	getBlob(base64) {
-		var arr = base64.split(','),
-			mime = arr[0].match(/:(.*?);/)[1],
-			bstr = atob(arr[1]),
-			n = bstr.length,
-			u8arr = new Uint8Array(n);
-		while (n--) {
-			u8arr[n] = bstr.charCodeAt(n);
-		}
-        return new Blob([u8arr], {type: mime});
-    },
-    doubleNumber(n) {
+    var week = ['日', '一', '二', '三', '四', '五', '六'];
+    return format.replace(/YYYY|YY|MM|DD|HH|hh|mm|SS|ss|week/g, function(key) {
+      switch (key) {
+        case 'YYYY':
+          return datetime.getFullYear();
+        case 'YY':
+          return (datetime.getFullYear() + '').slice(2);
+        case 'MM':
+          return datetime.getMonth() + 1 < 10 ? '0' + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+        case 'DD':
+          return datetime.getDate() < 10 ? '0' + datetime.getDate() : datetime.getDate();
+        case 'HH':
+        case 'hh':
+          return datetime.getHours() < 10 ? '0' + datetime.getHours() : datetime.getHours();
+        case 'mm':
+          return datetime.getMinutes() < 10 ? '0' + datetime.getMinutes() : datetime.getMinutes();
+        case 'SS':
+        case 'ss':
+          return datetime.getSeconds() < 10 ? '0' + datetime.getSeconds() : datetime.getSeconds();
+        case 'week':
+          return week[datetime.getDay()];
+      }
+    });
+  },
+  dataReplace(str, data) {
+    return str.replace(/{([^}]+)}/g, function(item, $1) {
+      if (data[$1]) return data[$1];
+      return item
+    });
+  },
+  formatCoin(number) {
+    number = (number * 1 || 0);
+    return (number.toFixed(2) + '').replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
+  },
+  getBase64(file, callback) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function(e) {
+      typeof(callback) == 'function' && callback(e.currentTarget.result);
+    }
+  },
+  getBlob(base64) {
+    var arr = base64.split(','),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {
+      type: mime
+    });
+  },
+  doubleNumber(n) {
     if (Number(n) < 10) return '0' + n;
     return n;
   },
